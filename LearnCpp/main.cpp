3546f3a309930f61f60f7ed31311a77ca042af23
TomWang <tom.wang@anker-in.com>
2022-08-12 15:15:21 +0800

    新建项目

---
+++ LearnCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearnCpp
+//
+//  Created by TomWang on 2022/8/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
